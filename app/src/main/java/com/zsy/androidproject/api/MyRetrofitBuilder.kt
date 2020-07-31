dc12b107aea4beee9c0f1832498ec6fbc0e87652
18580683725 <819879>
2020-07-31 17:44:03 +0800

    增加http请求日志输出

--- app/src/main/java/com/zsy/androidproject/api/MyRetrofitBuilder.kt
+++ app/src/main/java/com/zsy/androidproject/api/MyRetrofitBuilder.kt
@@ -1,13 +1,19 @@
 package com.zsy.androidproject.api
 
 import com.zsy.androidproject.BuildConfig
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object MyRetrofitBuilder {
     private val retrofitBuilder: Retrofit.Builder by lazy {
+        val logging = HttpLoggingInterceptor()
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY)
+        val client = OkHttpClient.Builder().addInterceptor(logging).build()
         Retrofit.Builder().baseUrl(BuildConfig.BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
     }
 
     val apiService: ApiService by lazy {
